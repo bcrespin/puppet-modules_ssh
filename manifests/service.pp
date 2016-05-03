642f340fc1426bfb2a81c2eb0941939060018299
brice <brice@bignas>
2016-05-03 11:33:59 +0200

    correct logical error

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,8 +1,8 @@
 class ssh::service inherits ssh{
-	service { '$service_name':
+	service { 'sshd':
     		ensure => running,
     		enable => true,
-		name => service_name,
+		name => $service_name,
 		hasstatus => true,
 		hasrestart => true,
   	}
