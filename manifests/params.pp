6ace70aadd80b395e278a33c02c3f558feb55c51
brice <brice@bignas>
2016-05-03 11:11:33 +0200

    correct issue : puppet do not allow reasign value to variable

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,19 +1,20 @@
 class ssh::params {
 
-$config = 'templates/sshd.conf.erb'
-$groupowner = 'root'
-$package_name = 'openssh-server'
-$service_name = 'sshd'
-
-
 case $operatingsystem {
 	'FreeBSD' :
 		{	$config = 'templates/FreeBSD_sshd.conf.erb'
 			$groupowner = 'wheel'
 			$package_name = 'BUILTIN'
+			$service_name = 'sshd'
+
 		}
 	'Default' : 
-		{ }
+		{
+		       $config = 'templates/FreeBSD_sshd.conf.erb'
+                        $groupowner = 'root'
+                        $package_name = 'openssh-server'
+                        $service_name = 'sshd'
+		 }
 }
 
 }
