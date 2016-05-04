7dd2e07cbd99dcbc517ab4f46302fec0002a7cd9
brice <brice@bignas>
2016-05-04 15:41:01 +0200

    correct check of key size

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,12 +13,16 @@ class ssh (
 
 # ensure $rsa_key_size,$ecdsa_key_size,$ed25519_key_size match required value
 $curve_key = ['256','384','512']
-
+$rsa_allowed_key = [ '1024' , '2048' , '4096']
 if  ! ($ecdsa_key_size in $curve_key)
 	{fail("ecdsa_key_size variable  has bad value") }
-
 if  ! ($ed25519_key_size in $curve_key)
         {fail("ed25519_size variable  has bad value") }
+if 
+
+if ! ($$rsa_key_size in $rsa_allowed_key)
+        {fail("rsa_key_size variable  has bad value") }
+if
 
 anchor { 'ssh::begin' : } ->
 class { 'ssh::install' : } ->
